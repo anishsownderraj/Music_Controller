21dc4f51ed96ff00525059214b51a1fe0ccdada4
Anish Sownderraj <anishsownderraj@Anishs-MBP.attlocal.net>
2021-08-17 23:42:58 -0700

    "Added All Files"

---
+++ API/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import RoomView, CreateRoomView
+urlpatterns = [
+   path('home', RoomView.as_view()),
+   path('createRoom', CreateRoomView.as_view())
+]
---
+++ frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { render } from  "react-dom";
+import HomePage from "./HomePage";
+
+export default class CreateRoomPage extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        return <p>This the Create Room Page</p>;
+    }
+}
+
